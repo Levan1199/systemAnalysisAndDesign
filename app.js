3ce5c8bbc7e2cca8c79535f9c133ebf57dde32e5
levan1199 <vipchicken28@gmail.com>
2021-05-16 18:29:25 +0700

    Add app use passport in app

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var employeeRouter = require('./routes/employee');
 var retailerReportRouter = require('./routes/retailerReport')
 var retailerRouter = require('./routes/retailer');
 
+var passport = require('passport');
 // Mongoose
 const mongoose = require('mongoose');
 
@@ -34,6 +35,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(passport.initialize());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
